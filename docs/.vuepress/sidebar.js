c2a2db22e1bb1e8df12b5b35b9adb8a9d4c60696
lxf <lixiaofan.dev@gmail.com>
2019-07-12 13:58:36 +0800

    ls touch cp 命令

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -8,7 +8,40 @@ module.exports = {
       ]
     }
 
-  ]
+  ],
+  '/learning/': [
+    {
+      title: 'linux',
+      collapsable: false,
+      children: [
+        ['linux/base', '基础知识']
+      ]
+    },
+    {
+      collapsable: false,
+      title: 'cpp',
+      children: [
+        ['cpp/a', 'a'],
+        ['cpp/库函数知识', '库函数知识'],
+
+      ]
+    },
+    {
+      collapsable: false,
+      title: 'webpack',
+      children: [
+        ['webpack/fundamental', '基础']
+      ]
+    },
+    {
+      collapsable: false,
+      title: 'nodejs',
+      children: [
+        ['nodejs/调试', '调试']
+      ]
+    }
+
+  ],
   // '/linux/': [
   //   '',      /* /bar/ */
   //   'a', /* /bar/three.html */
