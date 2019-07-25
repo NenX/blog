15ed20a3f18790c679214b5eae183d92ebb45360
lxf <lixiaofan.dev@gmail.com>
2019-07-25 11:23:47 +0800

    文件夹整理

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -6,25 +6,6 @@ module.exports = {
       children: [
         ['react/1', '1']
       ]
-    }
-
-  ],
-  '/learning/': [
-    {
-      title: 'linux',
-      collapsable: false,
-      children: [
-        ['linux/base', '基础知识']
-      ]
-    },
-    {
-      collapsable: false,
-      title: 'cpp',
-      children: [
-        ['cpp/a', 'a'],
-        ['cpp/库函数知识', '库函数知识'],
-
-      ]
     },
     {
       collapsable: false,
@@ -38,6 +19,25 @@ module.exports = {
       title: 'nodejs',
       children: [
         ['nodejs/调试', '调试']
+      ]
+    }
+
+  ],
+  '/learning/': [
+    {
+      title: 'linux',
+      collapsable: false,
+      children: [
+        ['linux/base', '基础知识'],
+      ]
+    },
+    {
+      collapsable: false,
+      title: 'cpp',
+      children: [
+        ['cpp/a', 'a'],
+        ['cpp/库函数知识', '库函数知识'],
+
       ]
     }
 
